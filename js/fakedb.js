d07040b1e9d25db407a58aa0055f07e068549cd3
leozito22 <leozitojava@hotmail.com>
2019-06-19 19:52:22 -0300

    Add files via upload

---
+++ js/fakedb.js
@@ -0,0 +1,260 @@
+// declara um conjunto fake de dados para contatos
+var dbJuliaLeo = {
+    "data": [
+        {
+            "id": 1,
+            "data": "13/02/2018 - 09h57",
+            "titulo": "Gossip Girl pode ganhar reboot",
+            "texto": `Durante painel no TCA, o presidente da emissora, Mark Pedowitz, não descartou a possibilidade de criar uma nova versão de Gossip Girl: "Existe uma discussão sobre o assunto, nada oficial. Mas não sei se já chegamos lá... Também não sei como seria. Muito depende da Warner Bros. e dos produtores Josh Schwartz e Stephanie Savage. Não gostaríamos de fazer algo sem eles." título da competição tire totalmente essa pressão, mas ainda é cedo.
+            Atualmente, a dupla de produtores ainda trabalha com a emissora em Dinastia, ao mesmo tempo que são responsáveis por Runaways no Hulu. Por enquanto, nenhum deles mostrou interesse no tal reboot e estão desenvolvendo a adaptação de Quem é Você, Alasca para o streaming. 
+            Exibida entre 2007 e 2012, Gossip Girl foi um sucesso de popularidade, retratando o início da obsessão pelas redes sociais e inspirando modas pelo mundo. A série revelou talentos como Blake Lively (vista em diversos filmes como Um Pequeno Favor), Leighton Meester (que agora estrela Single Parents) e Penn Badgley (protagonista de Você, sucesso da Netflix). Por sua vez, Ed Westwick foi acusado de estupro, mas a investigação não seguiu em frente.`,
+            "img": "/img/ggirl.jpg",
+            "curtidas": "79",
+            "curtido": false,
+            "likeColor": "black",
+            "comentarios": [
+                {
+                    "nome": "Eliza",
+                    "texto": "Só de pensar neste retorno eu já tenho arrepios!!!"
+                },
+                {
+                    "nome": "Julia",
+                    "texto": "Melhor serie do planeta"
+                },
+                {
+                    "nome": "Rommel",
+                    "texto": "Nunca fui com a cara destes personagens!"
+                },
+                {
+                    "nome": "José",
+                    "texto": "Fake news!"
+                }
+            ]
+        },
+        {
+            "id": 2,
+            "data": "15/06/2019 - 07h00",
+            "titulo": "Não há dúvida de que explosão de nave da SpaceX atrasará cronograma, diz NASA",
+            "texto": `Em abril, a nave Crew Dragon, da SpaceX, sofreu um incidente durante testes com seus motores de emergência. No mês seguinte, foi confirmada a explosão da nave por meio de vídeos que acabaram sendo divulgados no YouTube de maneira não oficial — só que, como a Crew Dragon é justamente uma das novas naves tripuláveis que, a partir de 2020, farão o transporte de astronautas dos Estados Unidos até a Estação Espacial Internacional (ISS), o destino do Commercial Crew Program ficou incerto.
+            Agora, Jim Bridenstine, administrador da NASA, veio a público admitir que a destruição da nave realmente causou problemas para o programa. "Não há dúvida de que o cronograma vai mudar, não será mais o que foi originalmente planejado", disse para jornalistas em uma palestra no Paris Airshow.
+            Os propulsores da nave que estavam sendo testados eram justamente os que entrariam em ação caso um voo precisasse ser abortado após o lançamento; eles afastam a cápsula com a tripulação do restante do veículo de lançamento, tudo para que os astronautas se mantenham seguros e consigam retornar à Terra. A SpaceX descreveu a falha ocorrida apenas como uma "anomalia", relevante o suficiente para causar a explosão. Contudo, o incidente ainda está em investigação tanto por parte da SpaceX, quanto por parte da própria NASA — a maior interessada nessa história.
+            Além da SpaceX, outra empresa privada que também transportará astronautas norte-americanos à ISS depois de 2020 é a Boeing, com sua nave Starliner — cujo desenvolvimento e testes também sofreram atrasos. A NASA tem urgência no início deste programa comercial pois há a necessidade de se independer dos russos, que, atualmente, fazem esse transporte por meio de suas naves Soyuz.`,
+            "img": "/img/spacex.jpeg",
+            "curtidas": "125",
+            "curtido": false,
+            "likeColor": "black",
+            "comentarios": [
+                {
+                    "nome": "Dayane",
+                    "texto": "Só atrapalham essas explosões :( "
+                },
+                {
+                    "nome": "Leonardo",
+                    "texto": "Ellon musk vai dar um jeito nisso"
+                },
+                {
+                    "nome": "João",
+                    "texto": "Esse povo vive no mundo da lua, hahahaha"
+                },
+                {
+                    "nome": "Iago",
+                    "texto": "Que piada horrorosa D:"
+                }
+            ]
+        },
+        {
+            "id": 3,
+            "data": "15/06/2019 - 07h20",
+            "titulo": "Por que batom de Marta pode gerar dor de cabeça com a FIFA",
+            "texto": `A Fifa tem seis patrocinadores oficiais e não permite que outras marcas façam propaganda de produtos durante seus eventos esportivos. A decisão de Marta de usar um batom da empresa Avon durante a vitória sobre a Itália levantou discussão: teria a camisa 10 infringido a regra da entidade máxima do futebol mundial?... 
+            O regulamento estipula dois tipos de "marketing de emboscada": por associação direta, quando a empresa em questão tenta ligar seu nome ao torneio e leva o público a pensar que é parceira da Fifa, e por "intrusão", quando a companhia aproveita a audiência de uma competição e convence um atleta a anunciar seu produto, muitas vezes, fingindo espontaneidade.`,
+            "img": "/img/marta.jpg",
+            "curtidas": "9",
+            "curtido": false,
+            "likeColor": "black",
+            "comentarios": [
+                {
+                    "nome": "Shirley Raio Laser",
+                    "texto": "WTF deixa ela usar o que quiser!"
+                },
+                {
+                    "nome": "Rafilds",
+                    "texto": "Sempre procurando meios de diminuir mulheres..."
+                }
+            ]
+        },
+        {
+            "id": 4,
+            "data": "18/06/2019 - 13h43",
+            "titulo": "Huawei promete reembolso caso Google e Facebook deixem seus aparelhos",
+            "texto": `A Huawei está recorrendo a medidas drásticas para neutralizar os efeitos de sua inclusão em uma lista negra do governo dos Estados Unidos, que resultou em várias empresas norte-americanas importantes cortando os laços com a gigante chinesa.
+            No entanto, as maiores baixas resultantes da proibição da Huawei estão relacionadas à perda iminente de acesso ao sistema operacional Android, da Google, e à loja de aplicativos da gigante das buscas. Enquanto isso, o Facebook também não está mais permitindo que a Huawei instale previamente seus aplicativos em seus telefones.
+            A Huawei, que atualmente é a segunda maior fabricante de smartphones do mundo, poderá, em breve, ser impedida de acessar aplicativos oficiais do Google, como YouTube e Gmail. Perder o acesso ao Android significaria, na prática, que nenhuma futura atualização do sistema operacional da Google — juntamente com as medidas de segurança que eles trazem — poderá ser aplicada nos aparelhos. Ao mesmo tempo, futuros dispositivos da Huawei também não poderão baixar aplicativos do Facebook, incluindo não apenas o Facebook, mas também o Instagram e o WhatsApp.
+            Nenhuma das opções acima, no entanto, afetaria os atuais modelos de celulares da marca chinesa, pelo menos imediatamente. Mas quando juntamos tudo isso com as notícias de que a empresa está prevendo internamente uma possível perda de receita de US$ 100 bilhões para o próximo biênio, bem como uma queda de 40% em seu negócio de smartphones, a garantia de reembolso que começa a fazer sentido.`,
+            "img": "/img/huawei.jpg",
+            "curtidas": "2",
+            "curtido": false,
+            "likeColor": "black",
+            "comentarios": [
+                {
+                    "nome": "Lee",
+                    "texto": "Trump maldito!"
+                },
+                {
+                    "nome": "Alexia",
+                    "texto": "Recalque americano"
+                },
+                {
+                    "nome": "Fernando",
+                    "texto": "O melhor do blog é que tem de tudo!"
+                },
+                {
+                    "nome": "Gustavo",
+                    "texto": "Puxa saco!"
+                }
+            ]
+        },
+        {
+            "id": 5,
+            "data": "17/06/2019 - 23h35",
+            "titulo": "Nintendo reforça que continuará dando suporte ao 3DS",
+            "texto": `Em entrevista à revista Time, o presidente da Nintendo of America, Doug Bowser, reforçou que sua empresa continuará dando suporte ao Nintendo 3DS.
+            Com o crescimento do Switch no mercado portátil, o 3DS parece ter ficado "de lado", recebendo poucas novidades nos últimos meses. O console nem deu as caras no Direct da E3 2019, focado inteiramente em jogos para o Switch: "Nesse momento, não temos jogos para anunciar para o 3DS, na verdade, nós não estamos mostrando conteúdo de 3DS aqui porque é inteiramente sobre Nintendo Switch", disse Bowser.
+            Mas nossos negócios com o 3DS seguem indo bem. Redes de varejo continuam a apoiar tanto o hardware quanto o software. Enquanto existir uma demanda de consumidores para o 3DS, nós continuaremos a apoiá-lo, completou o presidente.
+            Essa não é nem a primeira, nem a segunda vez que a Nintendo defende o 3DS. Em agosto de 2018, seu relatório de ganhos descrevia o console como um ótimo "primeiro contato" dos consumidores com a marca; seu preço, tamanho e peso também o colocariam em um nicho diferente do Switch.`,
+            "img": "/img/3ds.jpg",
+            "curtidas": "237",
+            "curtido": false,
+            "likeColor": "black",
+            "comentarios": [
+                {
+                    "nome": "Yaguchi",
+                    "texto": "Nintendo nunca me decepcionou"
+                },
+                {
+                    "nome": "Fernando",
+                    "texto": "Porque você nunca teve problema com cartuchos !"
+                }
+            ]
+        }
+    ]
+}
+
+// Caso exista no Local Storage, recupera os dados salvos
+var dbJuLeo = JSON.parse(localStorage.getItem('dbJuLeo'));
+if (!dbJuLeo) {
+    dbJuLeo = dbJuliaLeo;
+    localStorage.setItem('dbJuLeo',JSON.stringify(dbJuliaLeo));
+};
+
+// Exibe mensagem em um elemento de ID msg
+function displayMessage(msg) {
+    $('#msg').html('<div class="alert alert-warning">' + msg + '</div>');
+}
+
+function insertPostagem(postagem) 
+{
+    let novoId;
+
+    // Calcula novo Id a partir do último código existente no array
+    if (dbJuLeo.data.length == 0)
+        novoId = 0;
+    else 
+        novoId = dbJuLeo.data[dbJuLeo.data.length - 1].id + 1;
+    let data = new Date();
+    // Guarda cada pedaço em uma variável
+    let dia     = data.getDate();           // 1-31
+    let mes     = data.getMonth();          // 0-11 (zero=janeiro)
+    let ano    = data.getFullYear();       // 4 dígitos
+    let hora    = data.getHours();          // 0-23
+    let min     = data.getMinutes();        // 0-59
+
+    // Formata a data e a hora (note o mês + 1)
+    let str_data = dia + '/' + (mes+1) + '/' + ano;
+    let str_hora = hora + 'h' + min;
+    let data_completa = str_data + ' ' + str_hora;
+
+    let novaPostagem = {
+        "id": novoId,
+        "data": data_completa,
+        "titulo": postagem.titulo,
+        "texto": postagem.texto,
+        "img": postagem.img,
+        "curtidas": "0",
+        "curtido": false,
+        "likeColor": "black",
+        "comentarios": []
+    };
+
+    // Insere o novo objeto no array
+    dbJuLeo.data.push(novaPostagem);
+    displayMessage("Postagem inserida com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('dbJuLeo', JSON.stringify(dbJuLeo));
+}
+
+function insertComentario (id, comentario)
+{
+    let index = dbJuLeo.data.map(obj => obj.id).indexOf(id);
+
+    dbJuLeo.data[index].comentarios.push(comentario);
+    localStorage.setItem('dbJuLeo', JSON.stringify(dbJuLeo));
+}
+
+function updateCurtida (id, curtida, curtido, color)
+{
+    let index = dbJuLeo.data.map(obj => obj.id).indexOf(id);
+
+    dbJuLeo.data[index].titulo = dbJuLeo.data[index].titulo,
+    dbJuLeo.data[index].texto = dbJuLeo.data[index].texto,
+    dbJuLeo.data[index].img = dbJuLeo.data[index].img,
+    dbJuLeo.data[index].curtidas =parseInt( dbJuLeo.data[index].curtidas) + curtida;
+    dbJuLeo.data[index].curtido = curtido;
+    dbJuLeo.data[index].likeColor = color;
+    dbJuLeo.data[index].comentarios =  dbJuLeo.data[index].comentarios;
+
+    localStorage.setItem('dbJuLeo', JSON.stringify(dbJuLeo));
+
+}
+function updatePostagem(id, postagem) {
+    // Localiza o indice do objeto a ser alterado no array a partir do seu ID
+    let index = dbJuLeo.data.map(obj => obj.id).indexOf(id);
+
+    // Altera os dados do objeto no array
+    dbJuLeo.data[index].titulo = postagem.titulo,
+    dbJuLeo.data[index].texto = postagem.texto,
+    dbJuLeo.data[index].img = postagem.img,
+    dbJuLeo.data[index].curtidas = postagem.curtidas,
+    dbJuLeo.data[index].comentarios = postagem.comentarios
+
+    displayMessage("Postagem alterada com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('dbJuLeo', JSON.stringify(dbJuLeo));
+}
+
+function deletePostagem(id) {   
+    // Filtra o array removendo o elemento com o id passado
+    dbJuLeo.data = dbJuLeo.data.filter(function (element) { return element.id != id });
+
+    alert ("Postagem apagada com sucesso!");    
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('dbJuLeo', JSON.stringify(dbJuLeo));
+}
+
+/*function procuraPostagem(postagem) {
+    
+    for (i = 0; i < dbJuLeo.data.length; i++) {
+        var  listaPostagem = dbJuLeo.data[i];
+        if (listaUsuario.usuario == procurado.usuario)
+        {
+            if (listaUsuario.senha == procurado.senha)
+                return 1;
+            else
+                return 2;
+        }
+    }
+    return 3;
+}*/
